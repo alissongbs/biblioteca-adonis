e5e0ac2a8af93269e14983fa472dbb21de6583b0
Marcelo Damasceno <marcelodamasceno@gmail.com>
2025-10-17 09:46:37 -0300

    feat: implement Livro model and CRUD operations in LivrosController

--- app/controllers/livros_controller.ts
+++ app/controllers/livros_controller.ts
@@ -1,48 +1,68 @@
 import { createLivroValidator } from '#validators/livro'
 import type { HttpContext } from '@adonisjs/core/http'
-
+import Livro from "#models/livro"
+import { DateTime } from 'luxon'
 export default class LivrosController {
   /**
    * Display a list of resource
    */
   async index({}: HttpContext) {
-     return [
-      {id:1,titulo:"Pinochio",autor:'Disney',preco:"50"}
-    ]
+    const livros = await Livro.all()
+    return livros
   }
-
-  /**
-   * Display form to create a new record
-   */
-  async create({}: HttpContext) {}
 
   /**
    * Handle form submission for the create action
    */
   async store({ request }: HttpContext) {
     const payload = await request.validateUsing(createLivroValidator)
-    return payload
+    const data = {
+      ...payload,
+      dataPublicacao: DateTime.fromJSDate(payload.dataPublicacao as Date),
+    }
+    const livro = await Livro.create(data)
+    return livro
   }
 
   /**
    * Show individual record
    */
   async show({ params }: HttpContext) {
-    let codigo=params.id
+    const livro = await Livro.find(params.id)
+    return livro
   }
 
-  /**
-   * Edit individual record
-   */
-  async edit({ params }: HttpContext) {}
 
   /**
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request }: HttpContext) {
+    const livro = await Livro.find(params.id)
+    if (!livro) {
+      throw new Error('Livro não encontrado')
+    }
+
+    const payload = await request.validateUsing(createLivroValidator)
+    const data = {
+      ...payload,
+      dataPublicacao: DateTime.fromJSDate(payload.dataPublicacao as Date),
+    }
+
+    livro.merge(data)
+    await livro.save()
+    return livro
+  }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ params }: HttpContext) {
+    const livro = await Livro.find(params.id)
+    if (!livro) {
+      throw new Error('Livro não encontrado')
+    }
+
+    await livro.delete()
+    return { message: 'Livro deletado com sucesso' }
+  }
 }
